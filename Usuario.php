ac26a6a1cf004b2058a21fb9e608ebc26a1d4103
rocgar08 <35541675+rocgar08@users.noreply.github.com>
2018-04-23 20:49:19 +0200

    Update Usuario.php

--- Usuario.php
+++ Usuario.php
@@ -1,3 +1,49 @@
-<?php
-	class
+<?php 
+class Usuario{
+	  private $id;
+	  private $email;
+	  private $password;
+	  private $roles;
+	  private $fecha;
+	  private $nombre;
+	  private $nombreUsuario;
+
+	  private function __construct($id, $email, $password, $nombre, $nombreUsuario, $fecha) {
+		$this->id = $id;
+		$this->email = $email;
+		$this->password = $password;
+		$this->nombre = $nombre;
+		$this->nombreUsuario=$nombreUsuario;
+		$this->fecha = $fecha;
+		$this->roles = [];
+		}
+
+	  public function id() {
+	    return $this->id;
+	  }
+	  public function addRol($role) {
+	    $this->roles[] = $role;
+	  }
+	  public function roles() {
+	    return $this->roles;
+	  }
+	  public function email() {
+	    return $this->email;
+	  }
+	  public function fecha() {
+	    return $this->fecha;
+	  }
+	  public function nombre() {
+	    return $this->nombre;
+	  }
+	  public function nombreUsuario(){
+		return $this->nombreUsuario
+	  }
+	  public function compruebaPassword($password) {
+	    return password_verify($password, $this->password);
+	  }
+	  public function cambiaPassword($nuevoPassword) {
+	    $this->password = password_hash($nuevoPassword, PASSWORD_DEFAULT);
+	  }
+	}
 ?>
